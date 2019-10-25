a58bd52d7f9fa208d7b563f73bb49e90b1436963
TTTheo <wart_carson@yeah.net>
2019-10-24 22:43:34 -0400

    remove from master branch

--- jiang_zehui_Bank ATM/src/CheckingAccount.java
+++
@@ -1,8 +0,0 @@
-
-public class CheckingAccount extends Account{
-	public CheckingAccount(Bank bank, String userName, String id) {
-		super(bank, userName, id, "Checking", 20, 20);
-		// TODO Auto-generated constructor stub
-	}
-	
-}
--- jiang_zehui_Bank ATM/src/SavingAccount.java
+++
@@ -1,8 +0,0 @@
-
-public class SavingAccount extends Account{
-	public SavingAccount(Bank bank, String userName, String id) {
-		super(bank, userName, id, "Saving", 30, 30);
-		// TODO Auto-generated constructor stub
-	}
-}
-//public Account(String userName, String id, String type, double withdrawFee, double interest)
