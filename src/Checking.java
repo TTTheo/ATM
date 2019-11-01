8b713813c3351b28ef62626ce266ee0e60ca2cec
yujchen <chenyujingYW@163.com>
2019-11-01 00:47:37 -0400

    remove

--- src/Checking.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Checking extends CheckandSave{
-
-	public Checking(Account user,String accountNumber,String moneypassword,Balance balance){
-		super(user,accountNumber, moneypassword, balance);
-	}
-	
-}
