04aad619033f7b7e5d6487312125d049abb48d6b
TTTheo <wart_carson@yeah.net>
2019-11-08 22:55:55 -0500

    update db

--- src/Main.java
+++ src/Main.java
@@ -42,8 +42,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test() ;
-		// init();
+//		test() ;
+//		 init();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
